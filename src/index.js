f021ea43352d3eac37d4a7aa757dd2b5cc30035a
Mohammad Samiul Islam <forthright48@gmail.com>
2018-02-15 20:42:27 +0600

    Initial redux setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {reducers} from './reducers';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-
 import App from './App';
 
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(thunk),
+);
+
 ReactDOM.render((
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </Provider>
   ),
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+export function reducers(state=[], action) {
+  return state;
+}
