2856209021f043423e5c51536a6d7a30f4f2f906
Mohammad Samiul Islam <forthright48@gmail.com>
2018-03-06 21:17:31 +0600

    Add component for setting oj userid

--- src/components/userProfile/OJSolve.js
+++ src/components/userProfile/OJSolve.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {LinkContainer} from 'react-router-bootstrap';
-import {Row, Col, Table} from 'reactstrap';
+import {Row, Col, Table, Button, Input} from 'reactstrap';
 import {PropTypes} from 'prop-types';
 import Spinner from 'react-spinkit';
 import Loadable from 'react-loading-overlay';
@@ -10,8 +10,21 @@ export class OJSolve extends Component {
     super(props);
     this.state = {
       loading: false,
+      showInputForOj: '',
     };
     this.unsetOjUsername = this.unsetOjUsername.bind(this);
+    this.setUsername = this.setUsername.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
   }
 
   async updateSolveCount() {
@@ -40,7 +53,7 @@ export class OJSolve extends Component {
   }
 
   async unsetOjUsername(username, ojname) {
-    const {displayUser, updateOjStats} = this.props;
+    const {updateOjStats} = this.props;
 
     this.setState({
       loading: true,
@@ -56,19 +69,89 @@ export class OJSolve extends Component {
       if ( resp.status !== 201 ) throw resp;
 
       updateOjStats(resp.data);
-
-      this.setState({
-        loading: false,
-      });
     } catch (err) {
       if (err.status) alert(err.message);
       console.error(err);
     }
+    this.setState({
+      loading: false,
+    });
+  }
+
+  async setUsername(ojname) {
+    const {displayUser, updateOjStats} = this.props;
+    const {username} = displayUser;
+
+    this.setState({
+      loading: true,
+    });
+
+    const userId = this.state.ojusername;
+    try {
+      let resp = await fetch(`/api/v1/users/${username}/set-oj-username/${ojname}/${userId}`, {
+        method: 'PUT',
+        credentials: 'same-origin',
+      });
+      resp = await resp.json();
+      if ( resp.status !== 201) throw resp;
+      updateOjStats(resp.data);
+    } catch (err) {
+      if (err.status) alert(err.message);
+      console.error(err);
+    }
+
+    this.setState({
+      loading: false,
+    });
+  }
+
+  ojUsernameField(oj, index) {
+    const {displayUser, owner} = this.props;
+    const {username} = displayUser;
+    const {showInputForOj} = this.state;
+
+    const displayOjUsername = (
+      <span>
+        {oj.userIds[0]}
+        {owner? <i
+          className="fa fa-times text-danger ml-1 pointer"
+          onClick={()=>this.unsetOjUsername(username, oj.ojname)}
+        />: ''}
+      </span>
+    );
+
+    const inputForm = (
+      <div>
+        <input type='text' name='ojusername' onChange={this.handleInputChange}/>
+        <Button color='primary ml-1' onClick={()=>this.setUsername(oj.ojname)}>
+          Set
+        </Button>
+      </div>
+    );
+
+    const setUsername = (
+      <div>
+        { owner
+          ? (<div>
+              {showInputForOj === oj.ojname
+                ? inputForm
+                : <span
+                  className="btn-link pointer"
+                  onClick={()=>this.setState({showInputForOj: oj.ojname})}
+                  >
+                    Set Username
+                  </span>
+              }
+            </div>)
+          : <span>Not Set</span>
+      }
+    </div>
+  );
+    return oj.userIds[0]? displayOjUsername: setUsername;
   }
 
   render() {
-    const {displayUser, owner} = this.props;
-    const {username} = displayUser;
+    const {displayUser} = this.props;
     const ojStats = displayUser.ojStats;
     const ojSolve = ojStats? (
       <Table>
@@ -86,27 +169,7 @@ export class OJSolve extends Component {
               <tr key={oj._id}>
                 <td>{index}</td>
                 <td>{oj.ojname}</td>
-                <td>{oj.userIds[0]?(
-                  <span>
-                    {oj.userIds[0]}
-                    {owner? <i
-                      className="fa fa-times text-danger ml-1 pointer"
-                      onClick={()=>this.unsetOjUsername(username, oj.ojname)}
-                    />: ''}
-                  </span>
-                ):(
-                  <div>
-                    {
-                      owner?
-                      <LinkContainer to={`/users/profile/${username}/set-oj-username/${oj.ojname}`}>
-                        <span className="btn-link">
-                          Set Username
-                        </span>
-                      </LinkContainer>:
-                      <span>Not Set</span>
-                    }
-                  </div>
-                )}</td>
+                <td>{this.ojUsernameField(oj, index)}</td>
                 <td>{oj.solveCount}</td>
               </tr>
             );
